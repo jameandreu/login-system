c0d32a9cfdf060155dee61079dff08a6b8ebadbd
Andrew Ubag <jameandreu@gmail.com>
2022-09-03 22:02:01 +0800

    SCSS now working

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,48 @@
 
 <style lang="scss">
 	:global {
+		@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+		*,
+		*::before,
+		*::after {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		::-webkit-scrollbar {
+			width: 4px;
+			height: 4px;
+		}
+		::-webkit-scrollbar-track {
+			background-color: transparent;
+		}
+
+		::-webkit-scrollbar-thumb {
+			background-color: var(--color-brand);
+			border-radius: var(--radius-base);
+		}
+		:root {
+			--font-serif: 'Inter', sans-serif;
+		}
+		html,
 		body {
-			background-color: red;
+			height: 100vh;
+			font-family: var(--font-serif);
+			background-color: pink;
+			.someDiv {
+				height: 100%;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+
+				border: 1px solid red;
+				&__insideDiv {
+					width: 3rem;
+					height: 2rem;
+					background-color: aqua;
+				}
+			}
 		}
 	}
 </style>
---
+++ src/routes/Component.svelte
@@ -0,0 +1,20 @@
+<div class="component">
+	<div class="component__inside">some other text here</div>
+</div>
+
+<style lang="scss">
+	.component {
+		height: 20rem;
+		width: 30rem;
+		background-color: red;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		&__inside {
+			height: 10rem;
+			width: 20rem;
+			background-color: aqua;
+			text-align: center;
+		}
+	}
+</style>
